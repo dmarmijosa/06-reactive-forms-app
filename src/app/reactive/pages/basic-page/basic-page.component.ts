8fdf7c8f6649ae82e1941fa814eb4c0822468a34
Danny Armijos <daniel2667@hotmail.es>
2025-04-05 01:34:40 +0200

    feat: implementar formulario reactivo con validaciones y controles de entrada

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,25 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
 })
-export class BasicPageComponent { }
+export class BasicPageComponent {
+  // FormGroup is a class that represents a group of form controls
+  // myForm = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
+
+  private fb =  inject(FormBuilder);
+
+  myForm = this.fb.group({
+    name: ['',[] ],
+    price: [0],
+    inStorage: [0],
+  })
+}

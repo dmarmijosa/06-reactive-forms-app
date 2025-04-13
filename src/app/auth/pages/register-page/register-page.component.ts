757efb134c7f12a48cef0aa48f689ca22c06d7bd
Danny Armijos <daniel2667@hotmail.es>
2025-04-13 18:50:11 +0200

    feat: implementar formulario reactivo con validaciones y manejo de errores en la página de registro

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,8 +1,45 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './register-page.component.html',
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+  private fb = inject(FormBuilder);
+  formUtil = FormUtils;
+
+  myForm: FormGroup = this.fb.group({
+    name: [
+      '',
+      [Validators.required, Validators.pattern(this.formUtil.namePattern)],
+    ],
+    email: [
+      '',
+      [Validators.required, Validators.pattern(this.formUtil.emailPattern)],
+    ],
+    username: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.pattern(this.formUtil.notOnlySpacesPattern),
+      ],
+    ],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', Validators.required],
+  });
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+    if (this.myForm.invalid) return;
+    console.log(this.myForm.value);
+  }
+}
--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,5 +1,9 @@
 import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
+
 export class FormUtils {
+  static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+  static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
   static isValidField(form: FormGroup, fileName: string): boolean | null {
     return !!form.controls[fileName].errors && form.controls[fileName].touched;
   }
@@ -24,6 +28,10 @@ export class FormUtils {
     return this.getErrorMessages(error);
   }
   private static getErrorMessages(error: ValidationErrors) {
+    const patternMessages: { [key: string]: string } = {
+      [this.emailPattern]: 'El correo debe ser valido',
+      [this.notOnlySpacesPattern]: 'El username no debe tener espacios',
+    };
     for (const key of Object.keys(error)) {
       switch (key) {
         case 'required':
@@ -32,6 +40,12 @@ export class FormUtils {
           return `El campo debe tener al menos ${error[key].requiredLength} caracteres`;
         case 'min':
           return `El campo debe ser mayor a ${error[key].min}`;
+        case 'pattern':
+          const requiredPattern = error[key]?.requiredPattern;
+          if (requiredPattern && patternMessages[requiredPattern]) {
+            return patternMessages[requiredPattern];
+          }
+          return 'El formato del campo es inválido';
         default:
           return 'Campo inválido';
       }
